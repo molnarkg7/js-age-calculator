241acc7f51ebbd9e983bb5a2fd96ad939be1ec88
Đorđe Molnar <molnarkg7@gmail.com>
2023-11-21 11:13:45 +0100

    setInterval added (changing in real time)

--- script.js
+++ script.js
@@ -3,22 +3,25 @@ function reset() {
 }
 
 function calculate() {
-  let birthdate = new Date(document.getElementById("dateofbirth").value);
+  setInterval(() => {
+    let birthdate = new Date(document.getElementById("dateofbirth").value);
 
-  let now = new Date();
-  let ageInMs = now.getTime() - birthdate.getTime();
+    let now = new Date();
+    let ageInMs = now.getTime() - birthdate.getTime();
 
-  let ageInSec = ageInMs / 1000;
-  let ageInMin = ageInSec / 60;
-  let ageInHrs = ageInMin / 60;
-  let ageInDays = ageInHrs / 24;
-  let ageInMnts = ageInDays / 30.4;
-  let ageInYears = ageInMnts / 12;
+    let ageInSec = ageInMs / 1000;
+    let ageInMin = ageInSec / 60;
+    let ageInHrs = ageInMin / 60;
+    let ageInDays = ageInHrs / 24;
+    let ageInMnts = ageInDays / 30.4;
+    let ageInYears = ageInMnts / 12;
 
-  document.querySelector("#years").innerHTML = Math.floor(ageInYears);
-  document.querySelector("#months").innerHTML = Math.floor(ageInMnts % 12);
-  document.querySelector("#days").innerHTML = Math.floor(ageInDays % 30.4);
-  document.querySelector("#hours").innerHTML = Math.floor(ageInHrs % 24);
-  document.querySelector("#minutes").innerHTML = Math.floor(ageInMin % 60);
-  document.querySelector("#seconds").innerHTML = Math.floor(ageInSec % 60);
+    document.querySelector("#years").innerHTML = Math.floor(ageInYears);
+    document.querySelector("#months").innerHTML = Math.floor(ageInMnts % 12);
+    document.querySelector("#days").innerHTML = Math.floor(ageInDays % 30.4);
+    document.querySelector("#hours").innerHTML = Math.floor(ageInHrs % 24);
+    document.querySelector("#minutes").innerHTML = Math.floor(ageInMin % 60);
+    document.querySelector("#seconds").innerHTML = Math.floor(ageInSec % 60);
+    document.querySelector("#seconds");
+  }, 1000);
 }
